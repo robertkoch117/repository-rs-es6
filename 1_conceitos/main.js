76e8d03805767a6f05492cc06bc1a89bb6530a16
robertkoch117 <robertkoch117@gmail.com>
2020-03-17 11:30:56 -0300

    conceitos - classes 1

--- 1_conceitos/main.js
+++ 1_conceitos/main.js
@@ -1,8 +1,28 @@
-class Teste{
-    metodo(){
-
+class List{
+    constructor(){
+        this.data = [];
     }
-    outro(){
 
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
+}
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Robert';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
 }
