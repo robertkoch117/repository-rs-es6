db941c0a1945556cf7e35b1caf0eda523ce17b6f
robertkoch117 <robertkoch117@gmail.com>
2020-03-18 11:50:24 -0300

    conceitos operadores rest-spread pt4

--- bundle.js
+++ bundle.js
@@ -1,14 +1,17 @@
 "use strict";
 
 //REST
-function soma() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
+function soma(a, b) {
+  for (var _len = arguments.length, params = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    params[_key - 2] = arguments[_key];
   }
 
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
+  return params;
 }
 
-console.log(soma(1, 3, 4, 9, 1));
+console.log(soma(1, 3, 4, 4, 5, 6, 7)); //SPREAD
+
+var arr1 = [1, 2, 3];
+var arr2 = [4, 5, 6];
+var arr3 = [].concat(arr1, arr2);
+console.log(arr3);
--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 //REST
-function soma(...params){
-    return params.reduce((total, next)=>total + next);
+
+function soma(a,b,...params){
+    return params;
 }
 
-console.log(soma(1,3,4,9,1));
+console.log(soma(1,3,4,4,5,6,7));
+
+//SPREAD
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
