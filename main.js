44b8768e7d778ef200d896d658307587157a9dbd
robertkoch117 <robertkoch117@gmail.com>
2020-03-18 11:43:37 -0300

    conceitos operadores rest-spread pt3

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 //REST
+function soma(...params){
+    return params.reduce((total, next)=>total + next);
+}
 
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr;
-
-console.log(a);
-console.log(b);
-console.log(c);
+console.log(soma(1,3,4,9,1));
