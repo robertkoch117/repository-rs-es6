c4d42c4c1173a6a3479acf1e40a25d2aa79bafe9
robertkoch117 <robertkoch117@gmail.com>
2020-03-19 11:43:40 -0300

    aplicacao com es6+ - adicionar repositorios

---
+++ src/main.js
@@ -0,0 +1,28 @@
+class App{
+    constructor(){
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+
+        this.registerHandlers();
+    }
+
+    registerHandlers(){
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event){
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+        });
+
+        console.log(this.repositories);
+    }
+}
+
+new App();
