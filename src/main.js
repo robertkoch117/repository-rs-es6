7d00f20b7debd50eaff484a79fec647ad723ee48
robertkoch117 <robertkoch117@gmail.com>
2020-03-19 11:02:31 -0300

    async-await - async-await

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-alert('Teste 398');
+const minhaPromise = () => new Promise((resolve, reject)=>{
+    setTimeout(() => {resolve('OK')}, 2000);
+});
+
+//minhaPromise().then(response => ...);
+
+//async function executaPromise(){
+    //console.log(await minhaPromise());
+    //console.log(await minhaPromise());
+    //console.log(await minhaPromise());
+    
+    /*minhaPromise()..then(response => {
+        console.log(response);
+
+        minhaPromise().then(response => {
+            console.log(response);
+        })
+    })
+    */
+    
+//}
+
+const executaPromise = async() =>{
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+    console.log(await minhaPromise());
+}
+
+executaPromise();
